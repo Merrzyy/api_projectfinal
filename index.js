0ada0a079941e21977445da6b0a7eab3a091801e
Merrzyy <banknattayod@gmail.com>
2024-05-13 03:33:10 +0700

    new 2

--- index.js
+++ index.js
@@ -13,15 +13,6 @@ app.get('/', (req, res) => {
     res.send('Hello world!!')
 })
 
-app.get('/Movie', (req, res) => {
-  connection.query(
-      'SELECT * FROM Movie',
-      function (err, results, fields) {
-          res.send(results)
-      }
-  )
-})
-
 app.get('/users', (req, res) => {
     connection.query(
         'SELECT * FROM users',
@@ -74,6 +65,15 @@ app.delete('/users', (req, res) => {
             res.send(results)
         }
     )
+})
+
+app.get('/movie', (req, res) => {
+  connection.query(
+      'SELECT * FROM movie',
+      function (err, results, fields) {
+          res.send(results)
+      }
+  )
 })
 
 app.listen(process.env.PORT || 3000, () => {
